d1c4025e4a1c0e6d2893b794c671d4a747ec4370
Kirk Maat <kmaat@watrust.com>
2024-05-19 20:35:35 -0700

    draft 2 setup

--- src/player_pc.c
+++ src/player_pc.c
@@ -170,6 +170,8 @@ static const struct MenuAction gPCText_ItemPCOptionsText[] =
 static const struct ItemSlot gNewGamePCItems[] =
 {
     { ITEM_POTION, 1 },
+    { ITEM_MOON_STONE, 1},
+    { ITEM_LEAF_STONE, 1 },
     { ITEM_NONE, 0 }
 };
 
